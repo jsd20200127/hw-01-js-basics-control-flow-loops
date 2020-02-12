c1a03cb651a3cb34872ab769c94e73da54fc4355
Daniel Watson <dkwatson522gmail.com>
2020-02-12 18:41:33 -0500

    completed hw assignment

--- assignment.js
+++ assignment.js
@@ -32,6 +32,26 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+//our base number for multiplication table
+const baseNumber = 11
+
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+//
+// const baseNumber = getRandomInt(16)
+
+console.log(`Multiplication table for: ${baseNumber}`)
+
+//for loop to itterate through the first 12 numbers in the multiplication table
+for (let i = 0; i<=12; i++){
+  multiplicationTable = baseNumber * i
+  //console.log(`${i} - ${multiplicationTable}`)
+  console.log(multiplicationTable)
+}
+
+
 // Add your code above this line
 
 console.log('')
@@ -65,6 +85,16 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
+
+const numbers = [57, 13, 4, 38]
+
+//console.log(numbers)
+
+//for (let i = numbers[])
+
+let currentLargestNumber = Math.max(...numbers)
+
+console.log(currentLargestNumber)
 
 // Add your code above this line
 
@@ -112,6 +142,50 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+
+let playerAChoice = 'scissors'
+let playerBChoice = 'paper'
+
+// //array of possible choices
+// choices = ['paper','rock','scissors']
+//
+// //function to randomly choose index from choices array
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+//
+// let playerAChoice = choices[getRandomInt(3)]
+// let playerBChoice = choices[getRandomInt(3)]
+
+console.log(`playerA had ${playerAChoice} and playerB had ${playerBChoice}`)
+
+if (playerAChoice === playerBChoice) {
+  console.log("Game is a tie")
+}
+//playerA win opportunities
+else if (playerAChoice === 'rock' && playerBChoice === 'scissors') {
+  console.log("playerA wins")
+}
+else if (playerAChoice === 'paper' && playerBChoice === 'rock') {
+  console.log("playerA wins")
+}
+else if (playerAChoice === 'scissors' && playerBChoice === 'paper') {
+  console.log("playerA wins")
+}
+//playerB win opportunities
+else if (playerAChoice === 'rock' && playerBChoice === 'paper') {
+  console.log("playerB winss")
+}
+else if (playerAChoice === 'paper' && playerBChoice === 'scissors') {
+  console.log("playerB wins")
+}
+else if (playerAChoice === 'scissors' && playerBChoice === 'rock') {
+  console.log("playerB wins")
+}
+else {
+  console.log("Check player choices")
+}
+
 
 // Add your code above this line
 
